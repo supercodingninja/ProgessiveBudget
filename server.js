9124f0bd17f3a175f5e17278365c6905a14349ea
Frederick Thomas <supercodingninja@outlook.com>
2021-04-06 23:15:04 -0700

    Fixed all deprecated warnings

--- server.js
+++ server.js
@@ -1,27 +1,33 @@
-const express = require("express");
-const logger = require("morgan");
-const mongoose = require("mongoose");
-const compression = require("compression");
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const compression = require('compression');
 
 const PORT = 3000;
 
 const app = express();
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect('mongodb://localhost/budget', {
   useNewUrlParser: true,
   useFindAndModify: false
+
+  // These two  fixes all deprecated warnings.  Ref. https://mongoosejs.com/docs/deprecations.html
+  useCreateIndex: true,
+
+  // More Ref. https://mongodb.github.io/node-mongodb-native/3.3/reference/unified-topology/
+  useUnifiedTopology: true,
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require('./routes/api.js'));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
